6465b203f75dafa117746a1f1d4ba15058b54854
henriquelisboa2902 <henriquelisboa2902@gmail.com>
2025-03-30 18:51:43 +0000

    Ajustes do design responsivo e fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ export function App() {
 
   function handleSubmit() {
     if (rateNote !== 0) {
-    setSubmited(true)
-    return
+      setSubmited(true)
+      return
+    }
+
+    alert("Please, choose a rate button!")
   }
-  
-  alert("Please, choose a rate button!")
-}
 
   function handleChangeRateNote(value) {
     setRateNote(value)
   }
 
   return (
-    
-      submited === false ? (
-        
+
+    submited === false ? (
+
       <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl 
       font-overpass">
         <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
@@ -36,30 +36,30 @@ export function App() {
 
         <div className="flex justify-between mb-6">
           <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5
-          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white "onClick={() =>
-            handleChangeRateNote(1)} />
+          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400 "onClick={() =>
+              handleChangeRateNote(1)} />
           <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5
-          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white"onClick={() =>
-            handleChangeRateNote(2)} />
+          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400"onClick={() =>
+              handleChangeRateNote(2)} />
           <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5
-          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white"onClick={() =>
-            handleChangeRateNote(3)} />
+          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400"onClick={() =>
+              handleChangeRateNote(3)} />
           <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5
-          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white"onClick={() =>
-            handleChangeRateNote(4)} />
+          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400"onClick={() =>
+              handleChangeRateNote(4)} />
           <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5
-          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white"onClick={() =>
-            handleChangeRateNote(5)} />
+          rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400"onClick={() =>
+              handleChangeRateNote(5)} />
         </div>
 
         <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold
-        tracking-1" onClick={handleSubmit}>Submit</button>
+        tracking-1 hover:bg-white hover:text-orange cursor-pointer transition duration-400" onClick={handleSubmit}>Submit</button>
       </div>
-      ):(
-<div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl 
+    ) : (
+      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl 
       font-overpass text-center">
-        <img className="mx-auto mb-6" 
-        src={ilustrationThankYou} alt="Ilustration Thank You" />
+        <img className="mx-auto mb-6"
+          src={ilustrationThankYou} alt="Ilustration Thank You" />
 
         <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">
           You selectd {rateNote} out of 5</p>
@@ -68,7 +68,7 @@ export function App() {
 
         <p className="text-light-grey text-sm leading-1 mb-6">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
       </div>
-      )
-    
+    )
+
   )
 }
