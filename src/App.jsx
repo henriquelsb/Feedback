65bb6bab3308678f0a70192185f5f73eb009a5c9
henriquelisboa2902 <henriquelisboa2902@gmail.com>
2025-03-30 18:41:02 +0000

    finalização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -57,9 +57,16 @@ export function App() {
       </div>
       ):(
 <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl 
-      font-overpass">
-        <img src={ilustrationThankYou} alt="Ilustration Thank You" />
-        <p></p>
+      font-overpass text-center">
+        <img className="mx-auto mb-6" 
+        src={ilustrationThankYou} alt="Ilustration Thank You" />
+
+        <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">
+          You selectd {rateNote} out of 5</p>
+
+        <h1 className="font-bold text-2xl mb-2.5">Thank you!</h1>
+
+        <p className="text-light-grey text-sm leading-1 mb-6">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
       </div>
       )
     
