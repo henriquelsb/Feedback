85b4abe45f9a483a85b8e0bc4796e25a37d6d7e1
henriquelisboa2902 <henriquelisboa2902@gmail.com>
2025-03-30 18:16:47 +0000

    Criação da função para atualizar o card

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,62 @@
 import iconStar from "./assets/icon-star.svg"
+import { useState } from "react"
+
 export function App() {
+  const [submited, setSubmited] = useState(false)
+  const [rateNote, setRateNote] = useState(0)
+
+  function handleSubmit() {
+    if (rateNote !== 0) {
+    setSubmited(true)
+  }
+  
+  alert("Please, choose a rate button!")
+}
+
+  function handleChangeRateNote(value) {
+    setRateNote(value)
+  }
+
   return (
-    <div className="h-svh bg-very-dark-blue text-white flex justify-center items-center px-6">
-      <div className="bg-radial-gradient max-w-103">
-        <img src={iconStar} alt="ícone da estela" />
-
-        <h1>How did we do?</h1>
-
-        <p>Please let us know how we did with your support request. All feedback is appreciated to help us
-          improve our offering!</p>
-
-        <div>
-          <input type="button" value={1} />
-          <input type="button" value={2} />
-          <input type="button" value={3} />
-          <input type="button" value={4} />
-          <input type="button" value={5} />
+    <div className="h-svh bg-very-dark-blue text-white flex justify-center 
+    items-center px-6">
+      {submited === false ? (
+        
+      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl 
+      font-overpass">
+        <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
+          <img src={iconStar} alt="ícone da estela" />
         </div>
 
-        <button>Submit</button>
+        <h1 className="font-bold text-2xl mb-2.5">How did we do?</h1>
+
+        <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us
+          improve our offering!</p>
+
+        <div className="flex justify-between mb-6">
+          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5
+          rounded-full text-sm text-medium-grey font-bold focus:text-white O"onClick={() =>
+            handleChangeRateNote(1)} />
+          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5
+          rounded-full text-sm text-medium-grey font-bold focus:text-white O"onClick={() =>
+            handleChangeRateNote(2)} />
+          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5
+          rounded-full text-sm text-medium-grey font-bold focus:text-white O"onClick={() =>
+            handleChangeRateNote(3)} />
+          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5
+          rounded-full text-sm text-medium-grey font-bold focus:text-white O"onClick={() =>
+            handleChangeRateNote(4)} />
+          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5
+          rounded-full text-sm text-medium-grey font-bold focus:text-white O"onClick={() =>
+            handleChangeRateNote(5)} />
+        </div>
+
+        <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold
+        tracking-1" onClick={handleSubmit}>Submit</button>
       </div>
+      ):(
+<p>Outro Código</p>
+      )}
     </div>
   )
 }
